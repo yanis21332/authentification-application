8db797aa0af4d143d34efbd123a9ed86ccac95c0
yanis21332 <66674136+yanis21332@users.noreply.github.com>
2021-05-11 11:18:50 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import {Component} from 'react';
 import LoginCard from './component/LoginCard';
+import googleIcon from './elements/googleIcon.svg';
+import facebookIcon from './elements/facebookIcon.svg';
+import githubIcon from './elements/githubIcon.svg';
+import twitterIcon from './elements/twitterIcon.svg';
+import emailIcon from './elements/emailIcon.svg';
+import lockIcon from './elements/lockIcon.svg';
+import devChallenge from './elements/devchallenges.svg'
 
 class App extends Component
 {
   render(){
     return(
-      <LoginCard/>
+      <LoginCard devChallenge = {devChallenge} googleIcon = {googleIcon} facebookIcon = {facebookIcon} twitterIcon = {twitterIcon} githubIcon = {githubIcon} lockIcon = {lockIcon} emailIcon = {emailIcon} />
     )
   }
 }
--- src/component/LoginCard.js
+++ src/component/LoginCard.js
@@ -1,11 +1,11 @@
-
+import '../App.css'
 const LoginCard = props=>{
     return(
         <>
             <div className = "CardContainer">
                 <div className = "FirstPart">
                     <div className = "devchallengeZone">
-                        <img alt = "devchallenge" src = {props.devchallengelogo}/>
+                        <img alt = "devchallenge" src = {props.devChallenge}/>
                     </div>
                     <h2 className = "CardTitle">Join thousands of learners from around the world</h2>
                     <p className = "CardText">Master web devlopement by making real-life projects. There are multiple paths for you to choose</p>
@@ -13,18 +13,18 @@ const LoginCard = props=>{
                 <div className = "SecondPart FormPart">
                     <form>
                         <div className = "InputZone">
-                            <input className = "button emailButton" placeholder = "Email" name = "email" type = "email"/>
-                            <input className = "button passButton" placeholder = "Password" name = "password" type = "password"/>
+                            <img className = "InputIcon mailIcon" alt = "email" src = {props.emailIcon} /><input className = "button emailButton" placeholder = "Email" name = "email" type = "email"/>
+                            <br/><img alt = "passIcon InputIcon" src = {props.lockIcon} /><input className = "button passButton" placeholder = "Password" name = "password" type = "password"/>
 
-                            <input type = "submit" className = "submitButton"/>
+                            <input value = "Start coding now" type = "submit" className = "submitButton"/>
                         </div>
                         <p className = "anotherChoiceP transparentParagraphe">or continue with these social profile</p>
                         <div className = "LoginWithSocialMedia">
                              <div className = "socialMediaConnect">
-                                <div className = "GoogleLogin"><img alt = "google" src = {props.googleIcon}/></div>
-                                <div className = "FacebookLogin"><img alt = "facebook" src = {props.facebookIcon}/></div>
-                                <div className = "TwitterLogin"><img alt = "twitter" src = {props.twitterIcon}/></div>
-                                <div className = "GithubLogin"><img alt = "github" src = {props.githubIcon}/></div>
+                                <div className = "GoogleLogin mediaBox"><img alt = "google" src = {props.googleIcon}/></div>
+                                <div className = "FacebookLogin mediaBox"><img alt = "facebook" src = {props.facebookIcon}/></div>
+                                <div className = "TwitterLogin mediaBox"><img alt = "twitter" src = {props.twitterIcon}/></div>
+                                <div className = "GithubLogin mediaBox"><img alt = "github" src = {props.githubIcon}/></div>
                              </div>
                              <p className = "transparentParagraphe AlreadyMemberTexte">Already a member?<a href = "/Login" className = "BlueLoginButton">Login</a></p>
                         </div>
